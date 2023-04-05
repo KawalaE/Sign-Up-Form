bc375b011201a0d99a5c893d97774965b3a71402
KawalaE <edyta.wer.kawala@gmail.com>
2023-04-05 23:04:40 +0200

    Validate if inputs are empty

--- script.js
+++ script.js
@@ -1,4 +1,69 @@
-let password = document.getElementById('password').value;
-/*let passwordConfirmation = document.getElementById('password_confirm').value;*/
+let firstName = document.getElementById('user_name');
+let lastName = document.getElementById('user_last_name');
+let email = document.getElementById('email');
+let phone = document.getElementById('user_phone')
+let password = document.getElementById('password');
+let password2 = document.getElementById('password2');
+let form = document.getElementById('form');
+let message = document.getElementsByClassName('error');
 
-console.log(password);
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    inputCheck();
+});
+
+function isNameValid(e){
+    return  (/^[A-Za-z]+$/).test(String(e));
+}
+function isEmailValid(e){
+    return  (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(String(e));
+}
+function isPhoneValid(e){
+    return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3,6}$/).test(String(e));
+}
+
+function inputCheck(){
+    if(firstName.value === '' || !isNameValid(firstName.value)){
+        firstName.classList.add('fail');
+    } else{
+        firstName.classList.remove('fail');
+        firstName.classList.add('success');
+    }
+
+    if(lastName.value === ''|| !isNameValid(firstName.value)){
+        lastName.classList.add('fail');
+    }else{
+        lastName.classList.remove('fail');
+        lastName.classList.add('success');
+    }
+
+    if (email.value === '' || !isEmailValid(email.value)){
+        email.classList.add('fail');
+    }else{
+        email.classList.remove('fail');
+        email.classList.add('success');
+    }
+
+    if (phone.value === '' || !isPhoneValid(phone.value)){
+        phone.classList.add('fail');
+    }else{
+        phone.classList.remove('fail');
+        phone.classList.add('success');
+    }
+
+    if (password.value === ''){
+        password.classList.add('fail');
+    }else{
+        password.classList.remove('fail');
+        password.classList.add('success');
+    }
+
+    if (password2.value === ''){
+        password2.classList.add('fail');
+    }else{
+        password2.classList.remove('fail');
+        password2.classList.add('success');
+    } 
+}
+
