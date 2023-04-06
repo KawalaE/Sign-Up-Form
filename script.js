1fc9ea94a4a70c4ac232abf23a121b6f1127c966
KawalaE <edyta.wer.kawala@gmail.com>
2023-04-06 18:33:56 +0200

    Create messageHandler function

--- script.js
+++ script.js
@@ -22,48 +22,66 @@ function isEmailValid(e){
 function isPhoneValid(e){
     return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3,6}$/).test(String(e));
 }
+function mesageHandler(inputType, message){
+    let inputControl = inputType.parentElement;
+    let errorMessage = inputControl.querySelector('.error-msg');
+
+    errorMessage.innerText = message;
+}
 
 function inputCheck(){
-    if(firstName.value === '' || !isNameValid(firstName.value)){
+    if(firstName.value === ''){
         firstName.classList.add('fail');
+        errorHandler(firstName, "First name cannot be empty.")
     } else{
         firstName.classList.remove('fail');
         firstName.classList.add('success');
+        errorHandler(firstName, ' ');
     }
 
-    if(lastName.value === ''|| !isNameValid(firstName.value)){
+    if(lastName.value === ''){
         lastName.classList.add('fail');
+        errorHandler(lastName, "Last name cannot be empty.");
     }else{
         lastName.classList.remove('fail');
         lastName.classList.add('success');
+        errorHandler(lastName, ' ');
     }
 
     if (email.value === '' || !isEmailValid(email.value)){
         email.classList.add('fail');
+        errorHandler(email, "Email field cannot be empty.");
     }else{
         email.classList.remove('fail');
         email.classList.add('success');
+        errorHandler(email, ' ');
     }
 
-    if (phone.value === '' || !isPhoneValid(phone.value)){
+    if (phone.value === ''){
         phone.classList.add('fail');
+        errorHandler(phone, "Phone field cannot be empty.");
     }else{
         phone.classList.remove('fail');
         phone.classList.add('success');
+        errorHandler(phone, ' ');
     }
 
     if (password.value === ''){
         password.classList.add('fail');
+        errorHandler(password, "Password field cannot be empty.");
     }else{
         password.classList.remove('fail');
         password.classList.add('success');
+        errorHandler(password, ' ');
     }
 
     if (password2.value === ''){
         password2.classList.add('fail');
+        errorHandler(password2, "Password confirmation cannot be empty.");
     }else{
         password2.classList.remove('fail');
         password2.classList.add('success');
+        errorHandler(password2, ' ');
     } 
 }
 
