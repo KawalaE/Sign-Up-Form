8e6b1efc9dbc8d01df2cc2bfbe81303a4000ca13
KawalaE <edyta.wer.kawala@gmail.com>
2023-04-06 20:01:19 +0200

    Handle password confirmation

--- script.js
+++ script.js
@@ -22,66 +22,86 @@ function isEmailValid(e){
 function isPhoneValid(e){
     return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3,6}$/).test(String(e));
 }
-function mesageHandler(inputType, message){
+function messageHandler(inputType, message){
     let inputControl = inputType.parentElement;
     let errorMessage = inputControl.querySelector('.error-msg');
-
     errorMessage.innerText = message;
 }
 
 function inputCheck(){
     if(firstName.value === ''){
         firstName.classList.add('fail');
-        errorHandler(firstName, "First name cannot be empty.")
-    } else{
+        messageHandler(firstName, "First name is required.")
+    } else if(!isNameValid(firstName.value)){
+        firstName.classList.add('fail');
+        messageHandler(firstName, "Name must be alphabetic only.")
+    } 
+    else{
         firstName.classList.remove('fail');
         firstName.classList.add('success');
-        errorHandler(firstName, ' ');
+        messageHandler(firstName, ' ');
     }
 
     if(lastName.value === ''){
         lastName.classList.add('fail');
-        errorHandler(lastName, "Last name cannot be empty.");
+        messageHandler(lastName, "Last name is required.");
+    }else if(!isNameValid(lastName.value)){
+        lastName.classList.add('fail');
+        messageHandler(lastName, "Last name must be alphabetic only.")
     }else{
         lastName.classList.remove('fail');
         lastName.classList.add('success');
-        errorHandler(lastName, ' ');
+        messageHandler(lastName, ' ');
     }
 
-    if (email.value === '' || !isEmailValid(email.value)){
+    if (email.value === ''){
         email.classList.add('fail');
-        errorHandler(email, "Email field cannot be empty.");
-    }else{
+        messageHandler(email, "Email is required.");
+    }else if(!isEmailValid(email.value)){
+        email.classList.add('fail');
+        messageHandler(email, "Invalid email format.")
+    }
+    else{
         email.classList.remove('fail');
         email.classList.add('success');
-        errorHandler(email, ' ');
+        messageHandler(email, ' ');
     }
 
     if (phone.value === ''){
         phone.classList.add('fail');
-        errorHandler(phone, "Phone field cannot be empty.");
-    }else{
+        messageHandler(phone, "Phone field is required.");
+    }else if(!isPhoneValid(phone.value)){
+        phone.classList.add('fail');
+        messageHandler(phone, "Invalid phone number format.")
+    }
+    else{
         phone.classList.remove('fail');
         phone.classList.add('success');
-        errorHandler(phone, ' ');
+        messageHandler(phone, ' ');
     }
 
     if (password.value === ''){
         password.classList.add('fail');
-        errorHandler(password, "Password field cannot be empty.");
+        messageHandler(password, "Password is required.");
     }else{
         password.classList.remove('fail');
         password.classList.add('success');
-        errorHandler(password, ' ');
+        messageHandler(password, ' ');
     }
 
     if (password2.value === ''){
         password2.classList.add('fail');
-        errorHandler(password2, "Password confirmation cannot be empty.");
-    }else{
+        messageHandler(password2, "Password confirmation is required.");
+    } else if(password.value !== password2.value){
+        password2.classList.add('fail');
+        messageHandler(password2, "Passwords do not match.");
+        password.classList.add('fail');
+        messageHandler(password, "Passwords do not match.");
+    }
+    else{
         password2.classList.remove('fail');
         password2.classList.add('success');
-        errorHandler(password2, ' ');
+        messageHandler(password2, ' ');
     } 
 }
 
